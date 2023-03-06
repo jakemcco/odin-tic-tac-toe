7329fe8f5917497918b6b010392474251160e119
Jake McCormick <122489471+jakemcco@users.noreply.github.com>
2023-03-06 16:38:59 -0500

    Add reset button to index and button hover styling

---
+++ tic-tac-toe.js
@@ -0,0 +1,20 @@
+
+
+/* Input Handler */
+
+    /* Button event listeners
+        Grid buttons
+        New Game / Reset buttons    
+    */
+
+/* Game State Manager */
+    /* Game state array
+    /* Game state evaluator
+    /*
+
+/* Display Manager */
+    /*
+        Clear/reset grid
+        Update grid
+
+    */
